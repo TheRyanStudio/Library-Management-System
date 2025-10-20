0ae6313b8e880c4c2fa39db9ff24877a91aea8b8
Ryan Johnson <ryandjohnson08@hotmail.com>
2025-10-19 20:03:33 -0400

    R-CODE-1 - Implements application entry point for book collection initialization

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,10 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        // Initialize Library
+        InitializeLibrary library = new InitializeLibrary();
+        Collection collection = library.getCollection();
+        LibrarySystem system = new LibrarySystem(collection);
+
     }
 }
