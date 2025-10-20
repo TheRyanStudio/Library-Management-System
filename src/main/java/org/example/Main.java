367fa30e4b6b707961f3d623614c858c70242a8b
Ryan Johnson <ryandjohnson08@hotmail.com>
2025-10-19 20:22:49 -0400

    R-CODE-3 - Implements application entry point for user authentication and credentials validation.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -9,6 +12,30 @@ public class Main {
         Collection collection = library.getCollection();
         AccountManager accounts = library.getAccounts();
         LibrarySystem system = new LibrarySystem(collection);
+
+
+        PrintWriter output = new PrintWriter(System.out, true);
+        Scanner input = new Scanner(System.in);
+
+        output.println("Welcome to the Library Management System.");
+
+
+        while (true) {
+
+            // Login stage
+            system.promptForUsername(output);
+            String username = input.nextLine().trim();
+
+            system.promptForPassword(output);
+            String password = input.nextLine().trim();
+            Account authenticated = accounts.authenticate(username, password);
+
+            if (authenticated == null) {
+                system.displayAuthenticationError(output);
+                continue; // retry logging in
+            }
+
+        }
 
     }
 }
